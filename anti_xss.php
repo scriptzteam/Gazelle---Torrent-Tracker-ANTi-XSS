f0c398ce6c75025abdac98d1a2964fe71fbfab9e
[sCRiPTz-TEAM] <scriptzteam@users.noreply.github.com>
2017-03-19 12:13:34 +0100

    Create anti_xss.php

---
+++ anti_xss.php
@@ -0,0 +1,54 @@
+<?php
+
+function antiXSS($val) {
+   $val = preg_replace('/([\x00-\x08][\x0b-\x0c][\x0e-\x20])/', '', $val);
+   $search = 'abcdefghijklmnopqrstuvwxyz';
+   $search .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+   $search .= '1234567890!@#$%^&*()';
+   $search .= '~`";:?+/={}[]-_|\'\\';
+   for ($i = 0; $i < strlen($search); $i++) {
+      $val = preg_replace('/(&#[x|X]0{0,8}'.dechex(ord($search[$i])).';?)/i', $search[$i], $val); // with a ;
+      $val = preg_replace('/(&#0{0,8}'.ord($search[$i]).';?)/', $search[$i], $val); // with a ;
+   }
+   $ra1 = Array('javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'style', 'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'b$
+   $ra2 = Array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'on$
+   $ra = array_merge($ra1, $ra2);
+   $found = true;
+   while ($found == true) {
+      $val_before = $val;
+      for ($i = 0; $i < sizeof($ra); $i++) {
+         $pattern = '/';
+         for ($j = 0; $j < strlen($ra[$i]); $j++) {
+            if ($j > 0) {
+               $pattern .= '(';
+               $pattern .= '(&#[x|X]0{0,8}([9][a][b]);?)?';
+               $pattern .= '|(&#0{0,8}([9][10][13]);?)?';
+               $pattern .= ')?';
+            }
+            $pattern .= $ra[$i][$j];
+         }
+         $pattern .= '/i';
+         $replacement = substr($ra[$i], 0, 2).'<x>'.substr($ra[$i], 2);
+         $val = preg_replace($pattern, $replacement, $val);
+         if ($val_before == $val) {
+            $found = false;
+         }
+      }
+   }
+  return $val;
+}
+
+foreach($_GET as $input => $value) {
+    $_GET[$input] = antiXSS(strip_tags(preg_replace('/[^A-Za-z0-9\-]/', '', $value)));
+//echo "--".$_GET[$input]."<br>";
+}
+
+foreach($_POST as $input => $value) {
+    $_POST[$input] = antiXSS(strip_tags(preg_replace('/[^A-Za-z0-9\-]/', '', $value)));
+//echo "--".$_POST[$input]."<br>";
+}
+
+foreach($_REQUEST as $input => $value) {
+    $_REQUEST[$input] = antiXSS(strip_tags(preg_replace('/[^A-Za-z0-9\-]/', '', $value)));
+//echo "--".$_REQUEST[$input]."<br>";
+}
